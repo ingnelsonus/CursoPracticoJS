6aadb2b76ca2581ca31c3df311b165f06801d717
ingnelsonus <fabiousuga@gmail.com>
2022-03-10 10:18:57 -0500

    Figuras Geometricas

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # CursoPracticoJS
 Curso practico Basico JS
+
+## Taller #1: Figuras Geometricas.
+- Paso 1: Definir las formulas
+- paso 2: Implementar formulas en Java Script.
+- Paso 3: Crear funciones.
+-Paso 4: Integrar JS con HTML.
---
+++ figuras.js
@@ -0,0 +1,27 @@
+function perimetroCuadrado(lado){
+    return lado*4;
+}
+
+function AreaCuadrado(lado){
+    return lado*lado;
+}
+
+function perimetroTriangulo(lado1,lado2,ladoBase){
+    return lado1+lado2+ladoBase;
+}
+
+function AreaTriangulo(base,altura){
+    return (base*altura)/2;
+}
+
+function diametroCirculo(radio){
+    return radio*2;
+}
+
+function circunferenciaCirculo(radio){
+    return diametroCirculo(radio)*Math.PI;
+}
+
+function areaCirculo(radio){
+    return (radio*radio)*Math.PI;
+}
